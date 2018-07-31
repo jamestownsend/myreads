f2b12b213edd6a71043b6a80e429f0ac0895ccc6
James Townsend <jtownsend@pc.factset.com>
2018-07-31 10:20:18 +0100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import React from 'react'
-import ListBooks from "./ListBooks"
+import { Route } from "react-router-dom";
+import * as BooksAPI from "./BooksAPI";
+import ShowBooks from "./ShowBooks"
+import Search from "./Search";
 // import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
-  // state = {
-  //   /**
-  //    * TODO: Instead of using this state variable to keep track of which page
-  //    * we're on, use the URL in the browser's address bar. This will ensure that
-  //    * users can use the browser's back and forward buttons to navigate between
-  //    * pages, as well as provide a good URL they can bookmark and share.
-  //    */
-  //   showSearchPage: true
-  // };
+  state = {
+    books: []
+  };
+
+  componentDidMount() {
+    BooksAPI.getAll().then(books => {
+      this.setState({ books });
+    });
+  }
 
   render() {
     return (
-      <ListBooks></ListBooks>
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <ShowBooks/>)}/>
+        <Route path ="/search" render={() => (
+          <Search/>)}/>
+      </div>
+
     )
   }
 }
